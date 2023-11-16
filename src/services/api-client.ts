b565679c5578c7b3b4f3d17ce3ac4e21a75ac90a
olek-nixa <oleksandr@nixa.ca>
2023-11-16 17:13:22 -0500

    create custom hook to fetch the games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/apix",
+  baseURL: "https://api.rawg.io/api",
   params: {
     key: "8c694d83ef4c444aaf9fbaa5e1d02aa8",
   },
